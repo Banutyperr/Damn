f482d5e870b76b17425034c9575e8d1841e202a3
NifShams <140790034+NifShams@users.noreply.github.com>
2023-09-23 19:54:52 +0400

    Add files via upload
    The list of all Fibonacci k numbers .(K is inputted from the concole)

---
+++ fib.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void){
+    int k;
+    scanf ("%i",&k);
+    int fibonacci[k],i;
+    fibonacci[0]=0;
+    fibonacci[1]=1;
+    for (i=2;i<k;++i)
+        fibonacci[i]= fibonacci[i-1]+ fibonacci[i-2];
+    printf("The list of all Fibonacci %i numbers.\n",k);
+    for (i=0;i<k;++i)
+    printf("%2i \n",fibonacci[i]);
+    return 0;
+    }
