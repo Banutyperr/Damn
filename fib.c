af69f3c6236c537984a9db0c0202d7c1805906de
NifShams <140790034+NifShams@users.noreply.github.com>
2023-09-25 11:51:45 +0400

    Update fib.c

--- fib.c
+++ fib.c
@@ -12,3 +12,20 @@ int main(void){
     printf("%2i \n",fibonacci[i]);
     return 0;
     }
+//version 2
+#include <stdio.h>
+#include <stdbool.h>
+int main(void){
+int  FibF, k , FibS , FibC ;
+scanf( "%i", & k ) ;
+
+FibF =1;
+FibS =1;
+
+for (int i=0 ;i<k;++i)
+{
+    FibC=FibF+FibS;
+    FibF=FibS;
+    FibS=FibC;
+    printf("%i \n",FibC);
+}}
