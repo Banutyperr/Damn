6f0e8573ea6ae8752157c7aeb75077dcc4b8b49f
NifShams <140790034+NifShams@users.noreply.github.com>
2023-09-25 11:14:27 +0400

    Update main.c

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 *******************************************************************************/
 
 #include <stdio.h>
+#include <stdbool.h>
 int main (void)
 {
 int numReviews[11],review,i;
@@ -12,12 +13,16 @@ int numReviews[11],review,i;
 for (i=1;i<=10;++i)
 numReviews[i]=0;
 printf("Enter your reviews \n");
-for (i=1;i<=10;++i){
+printf("type in 999 to the console to finish entering the responses");
+while(true){
     scanf("%i",&review);
+      if (review==999)
+            break;
+      else{
     if(review<0||review>10)
     printf("Incorrect review \n");
     else
-    ++numReviews[review];
+    ++numReviews[review];}
     
 }
 printf("review      Num of reviews \n");
