e7802f8bfdd10b6bde773c625e6a7a96caba9208
NifShams <140790034+NifShams@users.noreply.github.com>
2023-09-27 22:46:30 +0400

    bubble sort.c

--- bubble sort.c
+++ bubble sort.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-void sort(float array[],int num){
+void sort(float array[],int num){//void cuz we will not need to return anything instead we will just change the array and print out different values of it
     int i,j;
     float temp;
-    for (i=0;i<num-1;++i){
+    for (i=0;i<num-1;++i){ //num-1 cuz we will not need to check the last element
         for (j=i+1;j<num;++j){
             if (array[i]>array[j]){
                 temp=array[i];
@@ -15,11 +15,11 @@ void sort(float array[],int num){
 int main(void){
     void sort(float array[],int num);
     int i;
-    float shams[16]={ 34.0, -5.0, 6.0, 0.0, 12.0, 100.0, 56.0, 22.0,
-44.0, -3.0, -9.0, 12.0, 17.0, 22.0, 6.0, 11.0 };
-sort(shams,16);
+    float Shams[16]={ 34.0, -5.0, 6.0, 0.0, 12.0, 100.0, 56.0, 22.0,
+44.0, -3.0, -9.0, 12.0, 17.0, 22.0, 6.0, 11.0 };//i am a leo,sorry(ghost emoji)
+sort(Shams,16);
 for(i=0;i<16;++i)
-printf("%.2f \n",shams[i]);
+printf("%.2f \n",Shams[i]);
 
 return 0;
     
