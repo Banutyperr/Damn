37d8110d192a85725925db36ffd5b8af3c670279
NifShams <140790034+NifShams@users.noreply.github.com>
2023-09-28 11:09:13 +0400

    factorial.c

--- factorial.c
+++ factorial.c
@@ -26,3 +26,42 @@ unsigned long int  factorial(unsigned int n){
     return result;
     
 }
+
+
+
+//2nd approach (i like it more( and not because of simplicity))
+
+
+#include <stdio.h>
+
+int main()
+{
+    unsigned int j;
+    int k;
+    scanf("%i",&k);
+    unsigned long int  factorial(unsigned int n);
+    for(j=0;j<k;++j)
+    printf ("%2u! = %lu \n", j, factorial (j));
+    
+    return 0;
+    
+}
+
+
+
+unsigned long int  factorial(unsigned int n){
+    unsigned long int result;
+    result=1;
+    int b;
+    
+    
+   for (b=1;b<=n;++b)
+   result*=b;
+    
+    return result;
+}
+
+
+//btw it breaks at k>65
+
+
