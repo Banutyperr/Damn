b3cdf1c877d5698930ac8a96f864e7e980fcd638
NifShams <140790034+NifShams@users.noreply.github.com>
2023-10-01 17:33:42 +0400

    Add files via upload

---
+++ INTTOROMAN .c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+   int num;
+   scanf("%i",&num);
+   
+
+   int integer[13]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+   char* rome[14]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+   
+   
+   for (int i = 0; i<13;++i){
+   while(num>=integer[i]){
+   num-=integer[i];
+   printf("%s",rome[i]);}}
+   
+    return 0;
+    
+}
