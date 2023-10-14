afb92fdbb56d6e2407e08a8b3f3d26d8f3f01eaa
NifShams <140790034+NifShams@users.noreply.github.com>
2023-10-15 00:14:13 +0400

    Add files via upload
    (Time complexity) O(n**26), but best case is still O(1) 
    Just to make sure that asymptotic notations are not just for best and worst case!It is just the analysis of a program (well i think )

---
+++ linear serch n^26 version.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+
+int main()
+{
+    int nenormalniy;
+    _Bool flag=0;
+    scanf("%i",&nenormalniy);
+    int nenujniy[6]={2,5,9,8,5,2};
+     #include <stdio.h>
+
+int main() {
+    for (int a = 0; a < 6; a++) {
+        for (int b = 0; b < 6; b++) {
+            for (int c = 0; c < 6; c++) {
+                for (int d = 0; d < 6; d++) {
+                    for (int e = 0; e < 6; e++) {
+                        for (int f = 0; f < 6; f++) {
+                            for (int g = 0; g < 6; g++) {
+                                for (int h = 0; h < 6; h++) {
+                                    for (int i = 0; i < 6; i++) {
+                                        for (int j = 0; j < 6; j++) {
+                                            for (int k = 0; k < 6; k++) {
+                                                for (int l = 0; l < 6; l++) {
+                                                    for (int m = 0; m < 6; m++) {
+                                                        for (int n = 0; n < 6; n++) {
+                                                            for (int o = 0; o < 6; o++) {
+                                                                for (int p = 0; p < 6; p++) {
+                                                                    for (int q = 0; q < 6; q++) {
+                                                                        for (int r = 0; r < 6; r++) {
+                                                                            for (int s = 0; s < 6; s++) {
+                                                                                for (int t = 0; t < 6; t++) {
+                                                                                    for (int u = 0; u < 6; u++) {
+                                                                                        for (int v = 0; v < 6; v++) {
+                                                                                            for (int w = 0; w < 6; w++) {
+                                                                                                for (int x = 0; x < 6; x++) {
+                                                                                                    for (int y = 0; y < 6; y++) {
+                                                                                                        for (int z = 0; z < 6; z++) {
+                                                                                                            if (nenujniy[z]==nenormalniy)
+                                                                                                            flag=1;
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+
+
+
+
+
+ if(flag)
+ printf("Vot eto povorot!");
+}
