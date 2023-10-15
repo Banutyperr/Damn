66c206db357089ab7b5bd8b7651d9ed41fac40b8
NifShams <140790034+NifShams@users.noreply.github.com>
2023-10-15 16:47:33 +0400

    fib.c

--- fib.c
+++ fib.c
@@ -2,13 +2,13 @@
 int main(void){
     int k;
     scanf ("%i",&k);
-    int fibonacci[k],i;
+    int fibonacci[k];
     fibonacci[0]=0;
     fibonacci[1]=1;
-    for (i=2;i<k;++i)
+    for (int i=2;i<k;++i)
         fibonacci[i]= fibonacci[i-1]+ fibonacci[i-2];
     printf("The list of all Fibonacci %i numbers.\n",k);
-    for (i=0;i<k;++i)
+    for (int i=0;i<k;++i)
     printf("%2i \n",fibonacci[i]);
     return 0;
     }
