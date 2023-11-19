2b92e086dda09879125b8c34220d68e1b4a95d99
NifShams <140790034+NifShams@users.noreply.github.com>
2023-11-19 17:32:46 +0400

    Update wordcount.c

--- wordcount.c
+++ wordcount.c
@@ -13,7 +13,7 @@ int  WordCount(const char string[]){
     int wordcount = 0 ;
     bool wordyes = true , alphabetic (const char c );
     for ( int i = 0 ; string [i] != '\0'; ++i ){
-        if ( alphabetic (string[i])){
+        if ( alphabetic (string[i])){//the beginning of the new word then it closes itself untill the nonword element then after it , it gives the key , and then after the starting of the other word it opens increments count and then closes itself again.
             if ( wordyes ){
                 ++wordcount;
                 wordyes = false;
