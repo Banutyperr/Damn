4e652ac9380a4903d19c7afdc0257be8f16b2d3e
NifShams <140790034+NifShams@users.noreply.github.com>
2023-12-01 13:33:03 +0400

    Add files via upload

---
+++ stringcopy2.0.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+void copyString( char *to , char *from){
+    while(*from)//unless from do not empty
+        *to++ = *from++;//first make them equal then increment 
+
+
+    *to = '\0';
+}
+
+int main() {
+
+    void copyString( char *to , char *from);
+
+    char hey[] = "hello bitch";
+    char bye[] ="good bye ";
+   copyString( hey , bye);//pointers to the first elements of an array
+
+
+
+    printf("%s",hey);
+
+    return 0;
+}
