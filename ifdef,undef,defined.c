4f4f43f574cb21cd278ed8bb1c5b8c6fa8aebcfb
NifShams <140790034+NifShams@users.noreply.github.com>
2023-12-11 22:21:11 +0400

    Add files via upload

---
+++ ifdef,undef,defined.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#ifndef hello
+#define hello
+
+
+
+//everything here will be included if hello was not defined
+
+#endif
+#define OS 1
+
+#if OS == 1
+#define k 6
+#elif OS==2
+#define k 7
+#elif  OS == 3
+#define k 8
+#endif
+
+#define name  7
+
+#if defined (name) // === ifdef name
+#define noname -name
+#endif
+
+
+#undef name
+
+
+
+
+int main(void) {
+    printf ("%i\n",k);
+    printf("%i\n",noname);
+    return 0;
+}
