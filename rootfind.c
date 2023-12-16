d46fec37df9886d20a7961119a024235e6d535bf
NifShams <140790034+NifShams@users.noreply.github.com>
2023-12-17 02:42:16 +0400

    Add files via upload

---
+++ rootfind.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+float absoluteValue (float x)
+{
+    if ( x < 0 )
+        x = -x;
+    return (x);
+}
+float squareRoot (float x)
+{
+    const float epsilon = .0000000001;
+    float guess = 1.0;
+    while ( absoluteValue (guess * guess - x) >= epsilon )
+        guess = ( x / guess + guess ) / 2.0;
+    return guess;
+}
+
+int main() {
+    double a , b, c ;
+    scanf("%lf",&a);
+    scanf("%lf",&b);
+    scanf("%lf",&c);
+    double x1,x2;
+
+
+
+
+   double discriminant = (b * b) - (4 * a * c) ;
+   printf("%lf\n",discriminant);
+   if (discriminant < 0 ){
+       printf("No real roots\n");
+   }
+   else {
+       b = -b ;
+       x1 = ( b  + squareRoot(discriminant) ) / (2 * a );
+       x2 = ( b - squareRoot(discriminant)) / (2 * a );
+       printf("%lf \n, %lf \n",x1, x2);
+       printf("2 roots\n");
+   }
+}
+
+
+
