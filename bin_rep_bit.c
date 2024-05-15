46e07fc1ccdf1bf46da0c7b0cf82ed9fc8ace8ea
NifShams <140790034+NifShams@users.noreply.github.com>
2024-05-16 01:37:34 +0400

    bin_rep_bit.c

--- bin_rep_bit.c
+++ bin_rep_bit.c
@@ -1,14 +1,15 @@
-
-
 int arr[32];
 
 void bin_rep_bit(int x) {
     for (int i = 31; i >= 0; --i) {
-        if ((x & (1 << i)) != 0) {
+        if ((x & (1ull << i)) != 0) {
             arr[31-i] = 1;
         } else {
             arr[31-i] = 0;
         }
     }
 }
+
+
+
 
