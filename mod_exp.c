ed61f8cfdf8ec4682a4d46cc24e8fa2b99a1e572
NifShams <140790034+NifShams@users.noreply.github.com>
2024-05-17 12:48:55 +0400

    mod_exp.c

--- mod_exp.c
+++ mod_exp.c
@@ -1,15 +1,15 @@
-
 #define l long long
-l mod_exp_fast(l a , l n , l m){
-    a = a % m ;
+l mod_exp_fast(l a , l b , l n){
+    a = a % n ; // a mod n 
     l r = 1 ;
-    while (n != 0){
-        if (n & 1){
-            r = (r * a) % m ;
+    while (b != 0){
+        if (b & 1){
+            r = (r * a) % n ; // {(r mod n ) * (a mod n )} mod n ==== ( a * r ) mod n 
         }
-        a = (a * a) % m ; 
-        n = n >> 1 ;
+        a = (a * a) % n ; // {(a mod n )*(a mod n)} mod n  ===== (a ^ 2) mod n 
+        b = b >> 1 ;
     }
     return r ; 
 }
 
+// (a * b) mod n = {(a mod n ) * ( b mod n )}mod n 
